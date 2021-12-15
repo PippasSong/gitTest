3f9d8e9d21558b6c0df9a79f4066cff95688e021
PippasSong <tlmag2108@gmail.com>
2021-12-15 09:18:17 +0900

    <FEAT> add eslint, prettier config file
    문법 교정, 스타일 통일을 위한 파일 추가

---
+++ test.js
@@ -0,0 +1,9 @@
+//https://velog.io/@recordboy/ESLint-Prettier-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+//https://feynubrick.github.io/2019/05/20/eslint-prettier.html
+function func() {
+  let foo = 'text';
+
+  return foo;
+}
+
+const a = 1;
